f08ead6f7e4f8575db10c136f9023f98b645133a
TemirlanSatvaldinov <ids.github.evu@gmail.com>
2023-07-17 19:41:09 +0600

    to snake case

--- src/matrix/mul.rs
+++ src/matrix/mul.rs
@@ -1,17 +1,17 @@
-pub fn matrix_mul(A:& [f32],row_a:usize,_col_a:usize,B:&[f32],row_b:usize,col_b:usize,C:&mut [f32]) {
+pub fn matrix_mul(a:& [f32],row_a:usize,_col_a:usize,b:&[f32],row_b:usize,col_b:usize,c:&mut [f32]) {
     
     //assert_eq!(col_a,row_b,"trying multiple {}x{} with {}x{}", row_a,col_a,row_b,col_b);
     
     for i in 0..row_a {
-        let  c = &mut C[i*col_b..];
+        let  slice_c = &mut c[i*col_b..];
         for j in 0..col_b {
-            c[j] = 0.0;
+            slice_c[j] = 0.0;
         }
         for k in 0..row_b {
-            let b = &B[k*col_b..];
-            let a = A[i*row_b+k];
+            let slice_b = &b[k*col_b..];
+            let x = a[i*row_b+k];
             for j in 0..col_b {
-                c[j] += a *b[j]
+                slice_c[j] += x *slice_b[j]
             }
         }
     }
