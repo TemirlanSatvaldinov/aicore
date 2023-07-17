930364c1840c601c66d42086574b65e2b9c1edb6
TemirlanSatvaldinov <ids.github.evu@gmail.com>
2023-07-17 19:14:47 +0600

    remove mut c

--- src/matrix/mul.rs
+++ src/matrix/mul.rs
@@ -1,9 +1,9 @@
-pub fn matrix_mul(A:& [f32],row_a:usize,col_a:usize,B:&[f32],row_b:usize,col_b:usize,C:&mut [f32]) {
+pub fn matrix_mul(A:& [f32],row_a:usize,_col_a:usize,B:&[f32],row_b:usize,col_b:usize,C:&mut [f32]) {
     
     //assert_eq!(col_a,row_b,"trying multiple {}x{} with {}x{}", row_a,col_a,row_b,col_b);
     
     for i in 0..row_a {
-        let mut c = &mut C[i*col_b..];
+        let  c = &mut C[i*col_b..];
         for j in 0..col_b {
             c[j] = 0.0;
         }
